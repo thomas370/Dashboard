5b6477d190fd811d6fa35f6f9fbd7c4b37303df7
Mathis Boulais <https://github.com/Asukyy>
2023-11-20 11:53:53 +0100

    icon

--- client/src/scenes/pie/index.jsx
+++ client/src/scenes/pie/index.jsx
@@ -5,7 +5,7 @@ import PieChart from "../../components/PieChart";
 const Pie = () => {
   return (
     <Box m="20px">
-      <Header title="Pie Chart" subtitle="Simple Pie Chart" />
+      <Header title="Pie Stats" subtitle="Admin Pie Stats" />
       <Box height="75vh">
         <PieChart />
       </Box>
